b1f801c146b94a44b9dcedf3226aec5936d78f31
Praanav-cpu <167501020+Aaditya120305@users.noreply.github.com>
2024-09-27 04:19:23 +0530

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="login-container">
+      <div className="login-box">
+        <div className="icon">
+          <img src="https://cdn-icons-png.flaticon.com/512/1077/1077114.png" alt="user icon" />
+        </div>
+        <h2>Login</h2>
+        <input type="text" placeholder="Enter Mobile Number" />
+        <input type="text" placeholder="Enter OTP" />
+        <div className="resend">
+          <a href="#">Resend OTP?</a>
+        </div>
+        <button>Login</button>
+        <div className="signup">
+          <a href="#">SignUP? If you don't have an account</a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
